3db131ce5f26140cb5ffb3fd620d6e5211e78102
Rodrigobernad89 <rodrigobernad89@gmail.com>
2021-12-14 14:36:01 -0300

    Creo barra de navegacion con sus respectivos estilos

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <header>     
+            <nav className="navBAr">
+                <ul className="menuDeNavegacion">
+                    <li><a href='#'>RB</a></li>  
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Productos</a></li>
+                    <li><a href='#'>Quienes Somos</a></li>
+                </ul> 
+            </nav> 
+        </header> 
+    )
+}
+
+export default NavBar
